3362978bf3c3af2d518f68caa0838e8b66a95c44
biq12 <khalid.biq.cod@gmail.com>
2018-05-28 00:49:01 +0000

    Tache Gestion de projets

--- BIHCENTER/BIHCENTER/Controllers/ProjetsController.cs
+++ BIHCENTER/BIHCENTER/Controllers/ProjetsController.cs
@@ -14,41 +14,27 @@ namespace BIHCENTER.Controllers
     {
         private BIHCENTERDBModel db = new BIHCENTERDBModel();
 
-        // GET: Projets
-        public ActionResult Index()
+       public ActionResult Index(int? page)
         {
-            var projets = db.Projets.Include(p => p.Admin);
-            return View(projets.ToList());
+            int pageSize = 12;
+            int pageNumber = (page ?? 1);
+            var projets = from s in db.Projets select s;
+            projets = projets.OrderBy(s => s.idProjet);
+            return View(projets.ToPagedList(pageNumber,pageSize));
         }
-
-        // GET: Projets/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Projet projet = db.Projets.Find(id);
-            if (projet == null)
-            {
-                return HttpNotFound();
-            }
-            return View(projet);
-        }
-
         // GET: Projets/Create
         public ActionResult Create()
         {
-            ViewBag.idAdmin = new SelectList(db.Admins, "id", "email");
             return View();
         }
 
         // POST: Projets/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "idProjet,NomProjet,dateDepart,dateFin,nomChefProjet,idAdmin")] Projet projet)
+        public ActionResult Create([Bind(Include = "idProjet,NomProjet,dateDepart,dateFin,nomChefProjet")] Projet projet)
         {
             if (ModelState.IsValid)
             {
@@ -57,76 +43,26 @@ namespace BIHCENTER.Controllers
                 return RedirectToAction("Index");
             }
 
-            ViewBag.idAdmin = new SelectList(db.Admins, "id", "email", projet.idAdmin);
             return View(projet);
         }
-
-        // GET: Projets/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Projet projet = db.Projets.Find(id);
-            if (projet == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.idAdmin = new SelectList(db.Admins, "id", "email", projet.idAdmin);
-            return View(projet);
-        }
-
-        // POST: Projets/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "idProjet,NomProjet,dateDepart,dateFin,nomChefProjet,idAdmin")] Projet projet)
+        public ActionResult Edit(int idProjet,string NomProjet,DateTime dateDepart,DateTime dateFin,string nomChefProjet)
         {
+            Projet projet = db.Projets.Find(idProjet);
+            projet.NomProjet = NomProjet;
+            projet.dateDepart = dateDepart;
+            projet.dateFin = dateFin;
+            projet.nomChefProjet = nomChefProjet;
             if (ModelState.IsValid)
             {
                 db.Entry(projet).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.idAdmin = new SelectList(db.Admins, "id", "email", projet.idAdmin);
-            return View(projet);
-        }
-
-        // GET: Projets/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Projet projet = db.Projets.Find(id);
-            if (projet == null)
-            {
-                return HttpNotFound();
-            }
-            return View(projet);
-        }
-
-        // POST: Projets/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Projet projet = db.Projets.Find(id);
-            db.Projets.Remove(projet);
-            db.SaveChanges();
             return RedirectToAction("Index");
         }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+     
+     
     }
 }
